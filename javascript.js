b1d9355f404263a39c727d7376fdd6fad56d1b10
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-11 16:23:39 -0800

    Add Gameboard module
    - Creates an array board of specified size.
    - Makes the placeToken and getBoard functions available.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const Gameboard = (function() {
+    const rows = 3;
+    const cols = 3;
+    const board = [];
+
+    // Create empty board
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < cols; j++) {
+            board[i].push(0);
+        }
+    }
+
+    const getBoard = () => board;
+
+    const placeToken = (row, col, player) => {
+        board[row][col] = player;
+    }
+
+    return { getBoard, placeToken };
+})();
