c9613e3b1cc168506a07ff86228d3b6138d35bba
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-11 20:17:47 -0800

    Add player and gameflow methods

--- javascript.js
+++ javascript.js
@@ -13,9 +13,70 @@ const Gameboard = (function() {
 
     const getBoard = () => board;
 
-    const placeToken = (row, col, player) => {
-        board[row][col] = player;
+    const placeToken = (row, col) => {
+        board[row][col] = Players.getActivePlayer().token;
     }
 
     return { getBoard, placeToken };
 })();
+
+const Players = (function(playerOne = 'Player 1', playerTwo = 'Player 2') {
+    const players = [
+        {
+            name: playerOne,
+            token: 1
+        },
+        {
+            name: playerTwo,
+            token: 2
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer;
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    return { getActivePlayer, switchPlayerTurn}
+})();
+
+
+
+const Gameflow = (function() {
+    const playRound = (row, col) => {
+        Gameboard.placeToken(row, col);
+        console.log(checkWin());
+        Players.switchPlayerTurn();
+    }
+
+    const checkWin = () => {
+        const board = Gameboard.getBoard();
+        const activeToken = Players.getActivePlayer().token;
+
+        for (i = 0; i < board.length; i++) {
+            let row = 0;
+            let col = 0;
+            let rightDiag = 0;
+            let leftDiag = 0;
+
+            let reducer = board.length - 1;
+            for (j = 0; j < board.length; j++) {
+                if (board[i][j] === activeToken) row++;
+                if (board[j][i] === activeToken) col++;
+                if (board[j][j] === activeToken) rightDiag++; 
+                if (board[j][reducer] === activeToken) leftDiag++;
+                reducer--;
+            }
+            if (row === board.length || col === board.length || rightDiag === board.length || leftDiag === board.length) return true;
+        }
+
+        return false
+    }
+
+    return { playRound };
+})();
+
+console.table(Gameboard.getBoard());
